cf275a1a5a3df1f92d43b9fbb7abf1ac4d59e4d2
ishanrana363 <ishanrana363@gmail.com>
2025-01-25 08:55:21 +0600

    added

--- src/Layout/AdminLayout.jsx
+++ src/Layout/AdminLayout.jsx
@@ -18,7 +18,7 @@ const AdminLayout = () => {
             {/* Right Sidebar */}
             <div className="w-[15%] bg-gray-800 text-white">
                 {/* Replace with appropriate content for the right sidebar */}
-                <div className="p-4">
+                <div className="p-4 mt-[200px]">
                     <h2 className="text-lg font-bold">Right Sidebar</h2>
                     <p>Additional tools or information here.</p>
                 </div>
